14a07f8a4a7e2786177c01a2f02043fcdc978452
JELLITE Oumayma <oumayma.jellite@etu.u-bordeaux.fr>
2019-03-19 10:10:38 +0100

    Component category

---
+++ web/front-end/src/app/components/admin/category/add-category/add-category.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import {Category} from '../../../../models/Category';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RegleService} from '../../../../services/regle.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent implements OnInit {
+  addcategory: FormGroup;
+  category: Category;
+  code: string;
+  name: string;
+  id: number;
+  error = false;
+
+  searchInput: string;
+  loading = {
+    status: false,
+    color: 'primary',
+    mode: 'indeterminate',
+    value: 50
+  };
+  constructor(private formBuilder: FormBuilder, private router: ActivatedRoute, private service: RegleService, private route: Router) { }
+
+  onSubmit() {
+    console.log('submit');
+    if (this.addcategory.invalid) {
+      this.error = true;
+      return;
+    }
+    console.log(this.addcategory.controls.regle.value);
+    const code = this.addcategory.controls.code.value;
+    console.log(code);
+    const name = this.addcategory.controls.name.value;
+    console.log(name);
+    this.category = new Category();
+  }
+
+  ngOnInit() {
+    this.addcategory = this.formBuilder.group({
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+
+    });
+    this.loading.status = true;
+  }
+
+}
