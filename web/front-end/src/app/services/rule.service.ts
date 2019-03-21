993b2a57e07473925db77fb8c805fcf97e57a232
Alfred Aboubacar SYLLA <a.aboubacar.sylla@gmail.com>
2019-03-21 22:14:06 +0100

    Liste Rule

--- web/front-end/src/app/services/rule.service.ts
+++ web/front-end/src/app/services/rule.service.ts
@@ -4,6 +4,7 @@ import {Observable, throwError} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import {Rule} from '../models/Rule';
 import {environment} from '../../environments/environment';
+import {Category} from "../models/Category";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,10 +17,22 @@ const httpOptions = {
 export class RuleService {
 
   private addRegleUrl = environment.BACK_END_URL + '/add/rule';
+  private getRulesUrl = environment.BACK_END_URL + '/get/rules';
+  private getRuleUrl = environment.BACK_END_URL + '/get/rule';
+  private getRulesCategoryUrl = environment.BACK_END_URL + '/get/rulesByCategory';
   constructor(private http: HttpClient) { }
 
   addRegle(regle: Rule) {
     return this.http.post<Response>(this.addRegleUrl, regle, httpOptions  );
+  }
+  getAllRules(): Observable<Rule[]> {
+    return this.http.get<Rule[]>(this.getRulesUrl);
+  }
+  getRulesByCategory(categoryId: number): Observable<Rule[]> {
+    return this.http.get<Rule[]>(this.getRulesCategoryUrl + '/' + categoryId);
+  }
+  getRule(ruleId: number) {
+    this.http.get<Rule>(this.getRuleUrl + '/' + ruleId);
   }
 
   private handleError(err: HttpErrorResponse) {
