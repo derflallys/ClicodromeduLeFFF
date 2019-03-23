730110768a00fc12070ebca57dcd41d6855fda25
Guillaume NEDELEC <guillaume.nedelec3308@gmail.com>
2019-03-23 02:04:23 +0100

    add category from add word form

--- web/front-end/src/app/components/admin/category/add-category/add-category.component.ts
+++ web/front-end/src/app/components/admin/category/add-category/add-category.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
 import {Category} from '../../../../models/Category';
 import {Router} from '@angular/router';
 import {CategoryService} from '../../../../services/category.service';
@@ -6,110 +7,111 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
 
 @Component({
-  selector: 'app-add-category',
-  templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.css']
+    selector: 'app-add-category',
+    templateUrl: './add-category.component.html',
+    styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent implements OnInit {
-  addCategory: FormGroup;
-  category: Category;
-  error = false;
-  title = 'Ajout d\'une nouvelle categorie';
-  errorRequest = false;
-  @Input() categoryId = null;
-  update  = false;
-  loading = {
-    status: false,
-    color: 'primary',
-    mode: 'indeterminate',
-    value: 50
-  };
-  saveRequest = false;
-  constructor(
-      private formBuilder: FormBuilder,
-      private service: CategoryService,
-      private route: Router,
-      public snackBar: MatSnackBar
-  ) { }
+    addCategory: FormGroup;
+    category: Category;
+    error = false;
+    title = 'Ajout d\'une nouvelle categorie';
+    errorRequest = false;
+    @Input() categoryId = null;
+    update  = false;
+    loading = {
+        status: false,
+        color: 'primary',
+        mode: 'indeterminate',
+        value: 50
+    };
+    saveRequest = false;
+    constructor(
+        private formBuilder: FormBuilder,
+        private service: CategoryService,
+        private route: Router,
+        public snackBar: MatSnackBar,
+        private location: Location
+    ) { }
 
-  onSubmit() {
-    console.log('submit');
-    if (this.addCategory.invalid) {
-      this.error = true;
-      return;
-    }
-    const code = this.addCategory.controls.code.value;
-    console.log(code);
-    const name = this.addCategory.controls.name.value;
-    console.log(name);
-    this.saveRequest = true;
-    const config = new MatSnackBarConfig();
-    config.verticalPosition = 'bottom';
-    config.horizontalPosition = 'center';
-    config.duration = 5000;
-    if (this.update) {
-      this.category = new Category(this.categoryId, code, name);
-      this.snackBar.open('⌛ Modification en cours...', 'Fermer', config);
-      this.service.updateCategory(this.category, this.category.id).subscribe(
-          response => {
-            console.log(response);
-            this.saveRequest = false;
-            this.snackBar.open('✅ Modification effectuée avec succès !', 'Fermer', config);
-            this.route.navigate(['/list/categories']);
-          }, error => {
+    onSubmit() {
+        console.log('submit');
+        if (this.addCategory.invalid) {
             this.error = true;
-            this.saveRequest = false;
-            if (error.status === 400) {
-              this.snackBar.open('❌ ' + error.error, 'Fermer', config);
-            }
-          }
-      );
-    } else {
-      this.category = new Category(null, code, name);
-      this.snackBar.open('⌛ Ajout en cours...', 'Fermer', config);
-      this.service.addCategory(this.category).subscribe(
-          response => {
-            this.saveRequest = false;
-            this.snackBar.open('✅ Ajout effectué avec succès !', 'Fermer', config);
-            this.route.navigate(['/list/categories']);
-          } , error => {
-            this.error = true;
-            this.saveRequest = false;
-            if (error.status === 400) {
-              this.snackBar.open('❌ ' + error.error, 'Fermer', config);
-            }
-          });
-    }
-    console.log(JSON.stringify(this.category));
-  }
-
-  ngOnInit() {
-    this.addCategory = this.formBuilder.group({
-      code: ['', Validators.required],
-      name: ['', Validators.required],
-    });
-    if (this.categoryId != null) {
-      this.loadData();
-    }
-  }
-
-  loadData() {
-    this.loading.status = true;
-    this.service.getCategory(this.categoryId).subscribe(
-        cat => {
-          this.category = cat;
-          this.title = 'Modification de la catégorie : ' + cat.name;
-          this.addCategory = this.formBuilder.group({
-            code: [cat.code, Validators.required],
-            name: [cat.name, Validators.required],
-          });
-          this.loading.status = false;
-          this.update = true;
-        }, error => {
-          this.loading.status = false;
-          this.errorRequest = true;
+            return;
         }
-    );
-  }
+        const code = this.addCategory.controls.code.value;
+        console.log(code);
+        const name = this.addCategory.controls.name.value;
+        console.log(name);
+        this.saveRequest = true;
+        const config = new MatSnackBarConfig();
+        config.verticalPosition = 'bottom';
+        config.horizontalPosition = 'center';
+        config.duration = 5000;
+        if (this.update) {
+            this.category = new Category(this.categoryId, code, name);
+            this.snackBar.open('⌛ Modification en cours...', 'Fermer', config);
+            this.service.updateCategory(this.category, this.category.id).subscribe(
+                response => {
+                    console.log(response);
+                    this.saveRequest = false;
+                    this.snackBar.open('✅ Modification effectuée avec succès !', 'Fermer', config);
+                    this.route.navigate(['/list/categories']);
+                }, error => {
+                    this.error = true;
+                    this.saveRequest = false;
+                    if (error.status === 400) {
+                        this.snackBar.open('❌ ' + error.error, 'Fermer', config);
+                    }
+                }
+            );
+        } else {
+            this.category = new Category(null, code, name);
+            this.snackBar.open('⌛ Ajout en cours...', 'Fermer', config);
+            this.service.addCategory(this.category).subscribe(
+                response => {
+                    this.saveRequest = false;
+                    this.snackBar.open('✅ Ajout effectué avec succès !', 'Fermer', config);
+                    this.location.back();
+                } , error => {
+                    this.error = true;
+                    this.saveRequest = false;
+                    if (error.status === 400) {
+                        this.snackBar.open('❌ ' + error.error, 'Fermer', config);
+                    }
+                });
+        }
+        console.log(JSON.stringify(this.category));
+    }
+
+    ngOnInit() {
+        this.addCategory = this.formBuilder.group({
+            code: ['', Validators.required],
+            name: ['', Validators.required],
+        });
+        if (this.categoryId != null) {
+            this.loadData();
+        }
+    }
+
+    loadData() {
+        this.loading.status = true;
+        this.service.getCategory(this.categoryId).subscribe(
+            cat => {
+                this.category = cat;
+                this.title = 'Modification de la catégorie : ' + cat.name;
+                this.addCategory = this.formBuilder.group({
+                    code: [cat.code, Validators.required],
+                    name: [cat.name, Validators.required],
+                });
+                this.loading.status = false;
+                this.update = true;
+            }, error => {
+                this.loading.status = false;
+                this.errorRequest = true;
+            }
+        );
+    }
 
 }
